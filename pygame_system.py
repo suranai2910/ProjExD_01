b6239cf9ef604d0053d8d64ce8c96f880cfbd654
c0b22147f6 <c0b22147f6@edu.teu.ac.jp>
2023-11-21 15:40:20 +0900

    青丸

--- pygame_system.py
+++ pygame_system.py
@@ -8,7 +8,7 @@ def main():
     font = pg.font.Font(None, 80)
 
     enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    pg.draw.circle(enn, (0, 0, 255), (10, 10), 10)
     enn.set_colorkey((0, 0, 0))
 
     tmr = 0
@@ -22,7 +22,7 @@ def main():
         screen.blit(enn, [100, 400])
         pg.display.update()
         tmr += 1        
-        clock.tick(1)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
