0d096601dba9383f1def6cbfbfda2d4a53a84111
c0b22147f6 <c0b22147f6@edu.teu.ac.jp>
2023-11-21 15:45:23 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -16,7 +16,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(tori_imgs[1], [300,200])
+        screen.blit(tori_imgs[tmr%2], [300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
