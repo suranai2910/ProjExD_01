e42ef1f5b0285220f1f3707067865971a61dc08f
c0b22147f6 <c0b22147f6@edu.teu.ac.jp>
2023-11-21 16:53:47 +0900

    演習課題２

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") # 練習1
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     tori_img = pg.image.load("ex01/fig/3.png") #練習２
     tori_img = pg.transform.flip(tori_img, True, False) #練習２
     tori_imgs = [tori_img, pg.transform.rotozoom(tori_img,2,1.0),
@@ -18,13 +19,14 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%1600) #練習６
+        x = -(tmr%3200) #練習６
         screen.blit(bg_img, [x, 0]) #練習６
-        screen.blit(bg_img, [1600+x,0]) #練習６
+        screen.blit(bg_img2, [1600+x,0]) #練習６
+        screen.blit(bg_img, [3200+x, 0])
         screen.blit(tori_imgs[tmr%10], [300,200]) #練習５
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(500)
 
 
 if __name__ == "__main__":
