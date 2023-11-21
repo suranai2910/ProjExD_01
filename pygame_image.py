3ff99e84684eced546311bd6b7fd17cdd1b9d679
c0b22147f6 <c0b22147f6@edu.teu.ac.jp>
2023-11-21 16:15:44 +0900

    演習課題１

--- pygame_image.py
+++ pygame_image.py
@@ -9,18 +9,22 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") # 練習1
     tori_img = pg.image.load("ex01/fig/3.png") #練習２
     tori_img = pg.transform.flip(tori_img, True, False) #練習２
-    tori_imgs = [tori_img, pg.transform.rotozoom(tori_img,10,1.0)] #練習３
+    tori_imgs = [tori_img, pg.transform.rotozoom(tori_img,2,1.0),
+                 pg.transform.rotozoom(tori_img,4,1.0), pg.transform.rotozoom(tori_img,6,1.0),
+                 pg.transform.rotozoom(tori_img,8,1.0),pg.transform.rotozoom(tori_img,10,1.0),
+                 pg.transform.rotozoom(tori_img,8,1.0),pg.transform.rotozoom(tori_img,6,1.0),
+                 pg.transform.rotozoom(tori_img,4,1.0),pg.transform.rotozoom(tori_img,2,1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%1600)
-        screen.blit(bg_img, [x, 0])
-        screen.blit(bg_img, [1600+x,0])
-        screen.blit(tori_imgs[tmr%2], [300,200])
+        x = -(tmr%1600) #練習６
+        screen.blit(bg_img, [x, 0]) #練習６
+        screen.blit(bg_img, [1600+x,0]) #練習６
+        screen.blit(tori_imgs[tmr%10], [300,200]) #練習５
         pg.display.update()
         tmr += 1        
-        clock.tick(1000)
+        clock.tick(10)
 
 
 if __name__ == "__main__":
